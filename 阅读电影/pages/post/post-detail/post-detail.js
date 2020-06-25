36984575569f81dc43209cd75dd28d55d8ea98ca
yangluoqiang <452566368@qq.com>
2020-06-25 18:27:44 +0800

    设置缓存成功

--- 阅读电影/pages/post/post-detail/post-detail.js
+++ 阅读电影/pages/post/post-detail/post-detail.js
@@ -29,18 +29,70 @@ Page({
       postscollected[postId] = false;
       wx.setStorageSync('posts_collected', postscollected)
     }
+
   },
   oncollectionTap: function (event) {
     var postscollected = wx.getStorageSync('posts_collected');
     var postcollected = postscollected[this.data.currentPostId];
 
     postcollected = !postcollected;
-    postscollected[this.data.currentPostId]=postcollected;
+    postscollected[this.data.currentPostId] = postcollected;
+    this.showModal(postcollected, postscollected)
+
+
+  },
+
+  showToast: function () {
+
     wx.setStorageSync('posts_collected', postscollected)
     this.setData({
-      collected: postcollected  
+      collected: postcollected
+    })
+    wx.showToast({
+      title: postcollected ? '收藏成功' : '取消收藏',
+      duration: 1000,
+      icon: 'loading'
     })
 
+  },
+  showModal: function (postcollected, postscollected) {
+    var taht = this;
+    wx.showModal({
+      cancelColor: '#333',
+      title: '收藏',
+      content: postcollected ? '收藏该文章？' : '取消收藏',
+      showCancel: true,
+      cancelText: '取消',
+      confirmText: '确认',
+      confirmColor: '#405f80',
+      success: function (res) {
+        if (res.confirm) {
+          wx.setStorageSync('posts_collected', postscollected)
+          taht.setData({
+            collected: postcollected
+          })
+        }
+      }
+    })
+  },
+  onshareTap: function (event) {
+    var itemList = [
+      '分享给微信好友',
+      '分享到朋友圈',
+      '分享到QQ',
+      '分享到微博'
+    ]
+    wx.showActionSheet({
+      itemList: itemList,
+      itemColor: '#405f80',
+      success: function (res) {
+        wx.showModal({
+          title: '用户' + itemList[res.tapIndex],
+          content: '现在无法实现'+res.cancel+'用户是否取消'
+        })
+      
+      }
+    }) 
   },
 
   /**
